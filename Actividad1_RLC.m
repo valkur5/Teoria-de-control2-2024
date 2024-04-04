1687297789413e60be33d9a0cfbfc0bbdc41c71d
Valentin Nieva Miguel <89718916+valkur5@users.noreply.github.com>
2024-04-03 21:10:10 -0300

    Update Actividad1_RLC.m
    Ejercicio 1 del RLC terminado

--- Actividad1_RLC.m
+++ Actividad1_RLC.m
@@ -2,8 +2,8 @@ pkg load control signal;
 clc; clear all; close all;
 
 %Variables de utilidad
-h=0.0001;
-tiempo=(0.1/h);
+h=1e-6; %Debe ser más chico que la frecuencia más rápida del sistema
+tiempo=(0.2/h);%2e5
 t=0:h:(tiempo*h);
 i=1;
 
@@ -11,7 +11,7 @@ i=1;
 R=47;
 L=1e-3;
 C=100e-6;
-Ve=12;
+Ve=0;
 I(1)=0;%X1
 V_c(1)=0;%X2
 
@@ -20,21 +20,32 @@ A=[-R/L -1/L; 1/C 0]
 B=[1/L; 0]
 Ct=[R 0]
 D=[0]
-X=[0 ; 0];
+X=[I ; V_c];
 u(1)=0;
-Sistema=ss(A,B,Ct,D);
+[num,den]=ss2tf(A,B,Ct,D);
+Y(1)=0;
 
 while(i<(tiempo+1))
   u(i)=Ve;
   I(i)=X(1);V_c(i)=X(2);
+  if( mod(i, 10000) == 0) %Cambia el valor de la tensión de alimentación cada 10 mil tics, que es 1ms
+    if (Ve==12)
+      Ve=-12;
+    else
+      Ve=12;
+    end
+  end
   X_P=A*X+B*u(i);%X punto
   X=X+h*X_P;%Esto es el cálculo de la integral como sumatoria
+
+  Y(i)=R*I(i);
   i=i+1;
 
 end
 
-I(i)=X(1);V_c(i)=X(2);
-
-subplot(2,1,1) ;plot(t,(I/max(I))); title("corriente");
-subplot(2,1,2) ;plot(t,V_c/max(V_c)); title("Tension capacitor");
-
+figure 1;
+subplot(3,1,1) ;plot(t,I); title("corriente"); grid on;
+subplot(3,1,2) ;plot(t,V_c); title("Tension capacitor");grid on;
+subplot(3,1,3) ;plot(t,u); title("Tension de entrada");grid on;
+figure 2;
+plot(t,Y);title("tension de la resistencia"); grid on;
